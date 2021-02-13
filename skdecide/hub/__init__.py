82aedcb9c7ec53483b73fb79271920642f4f4749
tog <guillaume.alleon_g@gmail.com>
2021-02-13 12:58:57 +0100

    fix import for the hub

--- skdecide/hub/__init__.py
+++ skdecide/hub/__init__.py
@@ -2,8 +2,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
 import fnmatch
+import os
+import sys
 
 for p in sys.path:
     for dirpath, dirs, files in os.walk(p):
